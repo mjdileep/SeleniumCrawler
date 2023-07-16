e3fac1cbb0cc6143738d02a0df8e0692e0db01d8
Dileep Jayamal <dileep.jayamal@gmail.com>
2023-07-16 22:21:01 +0530

    initial version

---
+++ JSCrawlerAPI.py
@@ -0,0 +1,60 @@
+# -*- coding:utf-8 -*-
+# (c) LombardGPT. All Rights Reserved
+#  Dileep Jayamal
+from selenium.webdriver.chrome.options import Options
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from selenium import webdriver
+import time
+
+chrome_options = Options()
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
+             '(KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+
+chrome_options.add_argument(f'user-agent={user_agent}')
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(options=chrome_options)
+
+
+token = "vLQja2SITLNdYQdphuMBer3423413213nj3n3jrnh3"
+
+
+class Link(BaseModel):
+    url: str
+    token: str
+
+
+app = FastAPI()
+
+
+@app.post("/get_page/")
+async def get_page(link: Link):
+    global driver
+    if link.token == token:
+        for i in range(2):
+            try:
+                driver.get(link.url.strip())
+
+                def page_has_loaded():
+                    page_state = driver.execute_script('return document.readyState;')
+                    return page_state == 'complete'
+                while not page_has_loaded():
+                    time.sleep(0.5)
+                return driver.page_source
+            except Exception as ex:
+                try:
+                    driver.close()
+                except:
+                    pass
+        return ""
+    return "Unauthorized!"
+
+
+if __name__ == '__main__':
+    uvicorn.run(app=app, host="0.0.0.0", port=8890)
+
