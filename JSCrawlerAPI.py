d9552f43fcc0a4ae1d4de6bdbcfc5c9e6788a7da
Dileep Jayamal <dileep.jayamal@gmail.com>
2023-07-22 13:53:40 +0530

    js page issue fixed

--- JSCrawlerAPI.py
+++ JSCrawlerAPI.py
@@ -19,7 +19,7 @@ user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
              '(KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 
 chrome_options.add_argument(f'user-agent={user_agent}')
-#chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
