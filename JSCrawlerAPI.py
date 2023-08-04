731601f96e56ad18f5bd6ac84f3bd5e65bd26d34
Dileep Jayamal <dileep.jayamal@gmail.com>
2023-08-04 22:10:43 +0530

    tokens removed

--- JSCrawlerAPI.py
+++ JSCrawlerAPI.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# (c) LombardGPT. All Rights Reserved
+# (c) All Rights Reserved
 #  Dileep Jayamal
 from selenium.webdriver.chrome.options import Options
 import uvicorn
@@ -16,7 +16,6 @@ from fastapi.exceptions import HTTPException
 from selenium.common.exceptions import TimeoutException
 from fastapi.responses import HTMLResponse
 from selenium.common.exceptions import InvalidSessionIdException
-import sys
 
 
 chrome_options = Options()
@@ -46,48 +45,40 @@ async def index():
 
 @app.post("/get_page/")
 async def get_page(link: Link):
-    global driver, token
-    print("Requested page:",link)
-    if link.token == token:
+    global driver
+    print("Requested page:", link)
+    try:
         try:
+            driver.get(link.url.strip())
+        except InvalidSessionIdException:
             try:
-                driver.get(link.url.strip())
-            except InvalidSessionIdException:
-                try:
-                    driver.close()
-                    driver.quit()
-                except:
-                    pass
-                driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-                driver.set_page_load_timeout(30)
-                driver.get(link.url.strip())
+                driver.close()
+                driver.quit()
+            except:
+                pass
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+            driver.set_page_load_timeout(30)
+            driver.get(link.url.strip())
 
-            t = time.time()
+        t = time.time()
 
-            def page_has_loaded():
-                page_state = driver.execute_script('return document.readyState;')
-                return page_state == 'complete'
-            while not page_has_loaded() and time.time()-t < 30:
-                time.sleep(0.5)
-            try:
-                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'a')))
-            except TimeoutException as ex:
-                print("Timeout:")
-                print(ex)
-            source = driver.page_source
-            return HTMLResponse(source)
-        except Exception as ex:
-            return HTTPException(status_code=501, detail="Error occurred")
-    return HTTPException(status_code=502, detail="Not allowed!")
+        def page_has_loaded():
+            page_state = driver.execute_script('return document.readyState;')
+            return page_state == 'complete'
+        while not page_has_loaded() and time.time()-t < 30:
+            time.sleep(0.5)
+        try:
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'a')))
+        except TimeoutException as ex:
+            print("Timeout:")
+            print(ex)
+        source = driver.page_source
+        return HTMLResponse(source)
+    except Exception as ex:
+        return HTTPException(status_code=501, detail="Error occurred")
 
 
 if __name__ == '__main__':
-    try:
-        workers = int(sys.argv[1])
-        token = sys.argv[2]
-    except:
-        workers = 5
-        token = "vLQja2SITLNdYQdphuMBer3423413213nj3n3jrnh3"
-    uvicorn.run(app='JSCrawlerAPI:app', host="0.0.0.0", port=8890, workers=workers)
+    uvicorn.run(app='JSCrawlerAPI:app', host="0.0.0.0", port=8890, workers=2)
 
